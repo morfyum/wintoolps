3a8b55e645755c85f93b6a0735b2fdd2c0d22b94
morfyum <morfyum@gmail.com>
2023-05-29 13:28:11 +0200

    fix WIN10ACTIVATOR

--- wintoolps.bat
+++ wintoolps.bat
@@ -1,35 +1,33 @@
 ECHO OFF
 CLS
 set policyStatus = FAIL
-
-ECHO WARNING! Press 1. to Enable scipt execution,
-ECHO after you finished, please press 2. to Disable script execution.
-ECHO STATUS: Unrestricted is NOT recommended, this is unsecure!
-
+ECHO ####################################################################
+ECHO # WARNING! Press 1. to Enable scipt execution,                     #
+ECHO # after you finished, please press 2. to Disable script execution. #
+ECHO # STATUS: Unrestricted is NOT recommended, this is unsecure!       #
+ECHO ####################################################################
 :MENU
 for /f %%i in ('powershell -Command "Get-ExecutionPolicy"') do set policyStatus=%%i
 ECHO.
-ECHO ========== ==========
-ECHO        CMD MENU
-ECHO ========== ==========
-ECHO STATUS: %policyStatus%
+ECHO ============= CMD MENU =============
+ECHO STATUS:   %policyStatus%
 ECHO.
 ECHO 0 - EXIT                       / 5 - Restore Health
-ECHO 1 - Enable Execute Scripts	    / 6 - Restart Network
-ECHO 2 - Disable to Execute Scripts	/ 7
+ECHO 1 - Enable Execute Scripts     / 6 - Restart Network
+ECHO 2 - Disable to Execute Scripts / 7
 ECHO 3 - Activate Windows 10        / 8
 ECHO 4 - Activate Windows 11        / 9
-ECHO ========== UNSTABLE APPS ==========
+ECHO ========== UNSTABLE TOOLS ==========
 ECHO 10 - service-manager
 ECHO.
 
 SET /P M=USE: 
 
-IF %M%==0 GOTO EOF
+IF %M%==0 GOTO EXIT
 IF %M%==1 GOTO EXPOLENABLE
 IF %M%==2 GOTO EXPOLDISABLE
-IF %M%==3 GOTO ACTIVATOR
-IF %M%==4 GOTO SERVICE-MANAGER
+IF %M%==3 GOTO W10ACTIVATOR
+IF %M%==4 GOTO W11ACTIVATOR
 IF %M%==5 GOTO RESTOREHEALTH
 IF %M%==6 GOTO RESTART-NETWORK
 IF %M%==10 GOTO SERVICE-MANAGER
@@ -88,3 +86,6 @@ cd %windir%\system32\calc.exe
 start calc.exe
 test&cls
 GOTO MENU
+
+:EXIT
+ECHO Exit 0 - Bye!
